d73fca136d7d939283e4091d150dba6879011a78
Mark Luoma <luomamr@gmail.com>
2019-03-11 16:40:47 -0400

    ok

--- server.js
+++ server.js
@@ -16,8 +16,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/ScrappyNews", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/ScrappyNews", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/ScrappyNews";
 
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
 // mongoose.connect(MONGODB_URI);
 // mongoose.connect(uristring, function (err, res) {
 //   if (err) {
