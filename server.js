0ea255328c48063381feb0b452abfbeb42385caa
Mark Luoma <luomamr@gmail.com>
2019-03-11 16:32:17 -0400

    first push

---
+++ server.js
@@ -0,0 +1,114 @@
+let express = require("express");
+let logger = require("morgan");
+let mongoose = require("mongoose");
+let axios = require("axios");
+let cheerio = require("cheerio");
+let db = require("./models");
+
+let PORT = 8000;
+
+let app = express();
+
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/ScrappyNews", { useNewUrlParser: true });
+
+// mongoose.connect(MONGODB_URI);
+// mongoose.connect(uristring, function (err, res) {
+//   if (err) {
+//   console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+//   } else {
+//   console.log ('Succeeded connected to: ' + uristring);
+//   }
+// });
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.golf.com/").then(function(response) {
+    let $ = cheerio.load(response.data);
+
+    $(".cell article").each(function(i, element) {
+      // Save an empty result object
+      let result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children(".four-up")
+        .text();
+      result.link = $(this)
+        .children(".four-up")
+        .attr("href");
+      result.summary = $(this)
+        .children(".art-summary")
+        .text();
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+    });
+
+    // Send a message to the client
+    res.send("Scrape Complete");
+  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("comment")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.comment.create(req.body)
+    .then(function(dbcomment) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbcomment._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
